2da78b1bc8e5e0484bdeb6c0f60c8d46797f673e
jmicoud <jacob.micoud@zenitel.com>
2023-07-27 15:44:49 +0100

    initial commit

---
+++ la.c
@@ -0,0 +1,255 @@
+#include "la.h"
+
+#include <float.h>
+#include <limits.h>
+#include <stdio.h>
+#include <string.h>
+
+#ifndef M_PI
+#define M_PI (3.14159265358979323846)
+#endif
+
+/**
+ * ----------------------------------------------------------------------------
+ */
+int feq(float f1, float f2) { return fabs(f1 - f2) < 0.0001f; }
+
+/**
+ * ----------------------------------------------------------------------------
+ */
+void printm4(const mat4_t m) {
+  printf("{ ");
+  for (int i = 0; i < 4; i++) {
+    printf("{ %f %f %f %f } ", m.elem[i][0], m.elem[i][1], m.elem[i][2],
+           m.elem[i][3]);
+  }
+  printf("}\n");
+}
+
+/**
+ * ----------------------------------------------------------------------------
+ */
+void printvn(const float *v, size_t n) {
+  printf("{ ");
+  for (size_t i = 0; i < n; i++) {
+    printf("%f ", v[i]);
+  }
+  printf("}\n");
+}
+
+/**
+ * ----------------------------------------------------------------------------
+ */
+float radians(const float degrees) { return (degrees * M_PI) / 180.0f; }
+
+/**
+ * ----------------------------------------------------------------------------
+ */
+mat4_t identitym4(void) {
+  mat4_t i = {.elem = {{1.0f, 0.0f, 0.0f, 0.0f},
+                       {0.0f, 1.0f, 0.0f, 0.0f},
+                       {0.0f, 0.0f, 1.0f, 0.0f},
+                       {0.0f, 0.0f, 0.0f, 1.0f}}};
+  return i;
+}
+
+/**
+ * ----------------------------------------------------------------------------
+ */
+vec3_t normalizev3(const vec3_t v) {
+  float l = sqrt(dotv3(v, v));
+  vec3_t n = {.elem = {v.elem[0] / l, v.elem[1] / l, v.elem[2] / l}};
+  return n;
+}
+
+/**
+ * ----------------------------------------------------------------------------
+ */
+float dotvn(const float *v1, const float *v2, size_t n) {
+  float res = 0.0f;
+  for (size_t i = 0; i < n; i++) {
+    res += v1[i] * v2[i];
+  }
+  return res;
+}
+
+/**
+ * ----------------------------------------------------------------------------
+ */
+float dotv3(const vec3_t v1, const vec3_t v2) {
+  return dotvn(v1.elem, v2.elem, 3);
+}
+
+/**
+ * ----------------------------------------------------------------------------
+ */
+int cmpv3(const vec3_t v1, const vec3_t v2) {
+  for (int i = 0; i < 3; i++) {
+    if (fabs(v1.elem[i] - v2.elem[i]) > FLT_EPSILON) {
+      return 0;
+    }
+  }
+  return 1;
+}
+
+/**
+ * ----------------------------------------------------------------------------
+ */
+int cmpv2(const vec2_t v1, const vec2_t v2) {
+  for (int i = 0; i < 2; i++) {
+    if (fabs(v1.elem[i] - v2.elem[i]) > FLT_EPSILON) {
+      return 0;
+    }
+  }
+  return 1;
+}
+
+/**
+ * ----------------------------------------------------------------------------
+ */
+float dotv4(const vec4_t v1, const vec4_t v2) {
+  return (v1.x * v2.x) + (v1.y * v2.y) + (v1.z * v2.z) + (v1.w * v2.w);
+}
+/**
+ * ----------------------------------------------------------------------------
+ */
+vec3_t crossv3(const struct vec3 v1, const struct vec3 v2) {
+  struct vec3 r = {
+      {{(v1.y * v2.z) - (v1.z * v2.y), (v1.z * v2.x) - (v1.x * v2.z),
+        (v1.x * v2.y) - (v1.y * v2.x)}}};
+  return r;
+}
+
+/**
+ * ----------------------------------------------------------------------------
+ */
+mat4_t productm4(const mat4_t m1, const mat4_t m2) {
+  mat4_t r = {0};
+  for (size_t i = 0; i < 4; i++) {
+    for (size_t j = 0; j < 4; j++) {
+      for (size_t k = 0; k < 4; k++) {
+        r.elem[i][k] += m1.elem[i][j] * m2.elem[j][k];
+      }
+    }
+  }
+  return r;
+}
+
+/**
+ * ----------------------------------------------------------------------------
+ */
+vec4_t productm4v4(const mat4_t m, const vec4_t v) {
+  vec4_t res = {0};
+  for (size_t i = 0; i < 4; i++) {
+    for (size_t j = 0; j < 4; j++) {
+      res.elem[i] += m.elem[i][j] * v.elem[j];
+    }
+  }
+  return res;
+}
+
+/**
+ * ----------------------------------------------------------------------------
+ */
+mat4_t perspective(const float fov, const float aspect_ratio, const float near,
+                   const float far) {
+  const float t = tan(fov / 2.0f);
+
+  mat4_t mat = {0};
+  mat.elem[0][0] = 1.0f / (aspect_ratio * t);
+  mat.elem[1][1] = 1.0f / t;
+  mat.elem[2][2] = -(far + near) / (far - near);
+  mat.elem[2][3] = -1.0f;
+  mat.elem[3][2] = -(2.0f * far * near) / (far - near);
+  return mat;
+}
+
+/**
+ * ----------------------------------------------------------------------------
+ */
+mat4_t look_at(const vec3_t eye, const vec3_t ctr, const vec3_t up) {
+  vec3_t f = {{{ctr.elem[0] - eye.elem[0], ctr.elem[1] - eye.elem[1],
+                ctr.elem[2] - eye.elem[2]}}};
+
+  f = normalizev3(f);
+  vec3_t s = normalizev3(crossv3(f, up));
+  vec3_t u = crossv3(s, f);
+
+  mat4_t mat = identitym4();
+
+  mat.elem[0][0] = s.elem[0];
+  mat.elem[1][0] = s.elem[1];
+  mat.elem[2][0] = s.elem[2];
+
+  mat.elem[0][1] = u.elem[0];
+  mat.elem[1][1] = u.elem[1];
+  mat.elem[2][1] = u.elem[2];
+
+  mat.elem[0][2] = -f.elem[0];
+  mat.elem[1][2] = -f.elem[1];
+  mat.elem[2][2] = -f.elem[2];
+
+  mat.elem[3][0] = -dotv3(s, eye);
+  mat.elem[3][1] = -dotv3(u, eye);
+  mat.elem[3][2] = dotv3(f, eye);
+  return mat;
+}
+
+/**
+ * ----------------------------------------------------------------------------
+ */
+mat4_t translate(const mat4_t m, const vec3_t v) {
+  mat4_t res = m;
+  res.elem[3][0] += v.elem[0];
+  res.elem[3][1] += v.elem[1];
+  res.elem[3][2] += v.elem[2];
+  return res;
+}
+
+/**
+ * ----------------------------------------------------------------------------
+ */
+mat4_t rotate(const mat4_t m, const vec3_t axis, const float rads) {
+  const float c = cos(rads);
+  const float s = sin(rads);
+
+  vec3_t a = normalizev3(axis);
+
+  mat4_t rot = {0};
+  rot.elem[0][0] = c + (1.0f - c) * a.elem[0] * a.elem[0];
+  rot.elem[0][1] = (1.0f - c) * a.elem[0] * a.elem[1] + s * a.elem[2];
+  rot.elem[0][2] = (1.0f - c) * a.elem[0] * a.elem[2] - s * a.elem[1];
+
+  rot.elem[1][0] = (1.0f - c) * a.elem[1] * a.elem[0] - s * a.elem[2];
+  rot.elem[1][1] = c + (1.0f - c) * a.elem[1] * a.elem[1];
+  rot.elem[1][2] = (1.0f - c) * a.elem[1] * a.elem[2] + s * a.elem[0];
+
+  rot.elem[2][0] = (1.0f - c) * a.elem[2] * a.elem[0] + s * a.elem[1];
+  rot.elem[2][1] = (1.0f - c) * a.elem[2] * a.elem[1] - s * a.elem[0];
+  rot.elem[2][2] = c + (1.0f - c) * a.elem[2] * a.elem[2];
+
+  rot.elem[3][3] = 1.0f;
+
+  // rot.elem[3][0] = m.elem[3][0];
+  // rot.elem[3][1] = m.elem[3][1];
+  // rot.elem[3][2] = m.elem[3][2];
+  // rot.elem[3][3] = m.elem[3][3];
+
+  return productm4(m, rot);
+}
+
+/**
+ * ----------------------------------------------------------------------------
+ */
+mat4_t scale(const mat4_t m, const vec3_t v) {
+  mat4_t sm = identitym4();
+  sm.elem[0][0] = v.elem[0];
+  sm.elem[1][1] = v.elem[1];
+  sm.elem[2][2] = v.elem[2];
+  sm.elem[3][0] = m.elem[3][0];
+  sm.elem[3][1] = m.elem[3][1];
+  sm.elem[3][2] = m.elem[3][2];
+  sm.elem[3][3] = m.elem[3][3];
+
+  return productm4(m, sm);
+}
---
+++ la.h
@@ -0,0 +1,197 @@
+#ifndef LIN_H_
+#define LIN_H_
+
+#include <math.h>
+#include <stddef.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif 
+
+int feq(float f1, float f2);
+
+typedef struct mat4 {
+  float elem[4][4];
+} mat4_t;
+
+typedef struct vec4 {
+  union {
+    struct {
+      float x;
+      float y;
+      float z;
+      float w;
+    };
+    float elem[4];
+  };
+} vec4_t;
+
+typedef struct vec3 {
+  union {
+    struct {
+      float x;
+      float y;
+      float z;
+    };
+    float elem[3];
+  };
+} vec3_t;
+
+typedef struct vec2 {
+  union {
+    struct {
+      float x;
+      float y;
+    };
+    float elem[2];
+  };
+} vec2_t;
+
+/**
+ * @brief Print a matrix to stdout.
+ *
+ * @param m The matrix to print.
+ */
+void printm4(const mat4_t m);
+
+/**
+ * @brief Print the contents of a vector.
+ *
+ * @param v The elements of the vector.
+ * @param n The number of dimensions of the vector.
+ */
+void printvn(const float *v, size_t n);
+
+/**
+ * @brief Convert an angle from degrees to radians. Not exact.
+ *
+ * @param degrees The angle in degrees.
+ * @return A floating point representing the angle in radians.
+ */
+float radians(const float degrees);
+
+/**
+ * @brief Make a mat4 into an identity matrix.
+ *
+ * @param m The mat4.
+ */
+mat4_t identitym4(void);
+
+/**
+ * @brief Normalize a vec3.
+ *
+ * @param vec The vec3 to normalize.
+ * @return A normalized vec.
+ */
+vec3_t normalizev3(const vec3_t vec);
+
+/**
+ * @brief Get the dot product of 2 vec3's.
+ *
+ * @param v1 The first vec3.
+ * @param v2 The second vec3.
+ * @return The dot product of v1 and v2.
+ */
+float dotv3(const vec3_t v1, const vec3_t v2);
+
+/**
+ * @brief Compare 2 vec3s
+ * @param v1
+ * @param v2
+ * @return 1 if the vectors are the same, otherwise 0.
+ */
+int cmpv3(const vec3_t v1, const vec3_t v2);
+
+/**
+ * @brief Compare 2 vec2s
+ * @param v1
+ * @param v2
+ * @return 1 if the vectors are the same, otherwise 0.
+ */
+int cmpv2(const vec2_t v1, const vec2_t v2);
+
+/**
+ * @brief Get the dot product of 2 vec4's
+ *
+ * @param v1 The first vec4.
+ * @param v2 The second vec4.
+ * @return The dot product of v1 and v2.
+ */
+float dotv4(const vec4_t v1, const vec4_t v2);
+
+/**
+ * @brief Cross 2 vec3's.
+ *
+ * @param v1 The first vec3.
+ * @param v2 The second vec3.
+ * @return The cross product of v1 and v2.
+ */
+vec3_t crossv3(const vec3_t v1, const vec3_t v2);
+
+/**
+ * @brief Get the product of 2 4 x 4 matrices
+ *
+ * @param m1 The first mat4.
+ * @param m2 The second mat4.
+ * @return The product of m1 and m2.
+ */
+mat4_t productm4(const mat4_t m1, const mat4_t m2);
+
+/**
+ * @brief Get the product of a 4x4 matrix and a vec4.
+ *
+ * @param m The matrix.
+ * @param v The vector.
+ * @return The product of m and v
+ */
+vec4_t productm4v4(const mat4_t m, const vec4_t v);
+
+/**
+ * @brief TODO
+ */
+mat4_t perspective(const float fov, const float aspect_ratio, const float near,
+                   const float far);
+
+/**
+ * @brief Create a view matrix.
+ *
+ * @param eye The direction of the view.
+ * @param ctr The position of the view.
+ * @param up The up direction of the view.
+ * @return A view matrix.
+ */
+mat4_t look_at(const vec3_t eye, const vec3_t ctr, const vec3_t up);
+
+/**
+ * @brief Translate a mat4 by a vec3.
+ *
+ * @param m The matrix to translate.
+ * @param v The translation vector.
+ * @return The translated matrix.
+ */
+mat4_t translate(const mat4_t m, const vec3_t v);
+
+/**
+ * @brief Rotate a matrix about and axis (using unit quaternion).
+ *
+ * @param m The matrix to apply the rotation to.
+ * @param axis The axis of the rotation.
+ * @param rads The rotation angle in radians.
+ * @return The rotated matrix.
+ */
+mat4_t rotate(const mat4_t m, const vec3_t axis, const float rads);
+
+/**
+ * @brief Scale a transformation matrix by a vector v.
+ *
+ * @param m The matrix to transform.
+ * @param v The scaling vector.
+ * @return The scaled matrix.
+ */
+mat4_t scale(const mat4_t m, const vec3_t v);
+
+#ifdef __cplusplus
+}
+#endif 
+
+#endif  // LIN_H_
